b033459b59eab44060de174665ead7da6f8ccbac
Maniha Rasheed <maniharasheed123@gmail.com>
2024-08-28 13:01:56 +0500

    pP

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,14 +12,7 @@ const config: PlaywrightTestConfig = {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+
   ],
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
